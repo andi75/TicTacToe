4664e63baa9737015efabc5d02d4564f954b5ee6
Andreas Umbach <umbach@gmail.com>
2015-08-09 14:39:05 +0200

    - initial non-functioning state

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBInspectable var circleColor = UIColor.redColor()
+    @IBInspectable var crossColor = UIColor.blueColor()
+    
+    var isCircleTurn = true
+    
+    @IBOutlet var ticTacViewCollection: [TickTackView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        let tapReg = UITapGestureRecognizer(target: self, action: "tickTackViewTapped:")
+        var index: Int = 0
+        for view in ticTacViewCollection
+        {
+            view.type = TickTackType.Empty
+            view.addGestureRecognizer(tapReg)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tickTackViewTapped(tappedObj: AnyObject)
+    {
+        let ttV = tappedObj as? TickTackView
+        if(ttV != nil && ttV!.type == TickTackType.Empty)
+        {
+            if(self.isCircleTurn)
+            {
+                ttV!.type = TickTackType.Circle
+                ttV!.elementColor = circleColor
+            }
+            else
+            {
+                ttV!.type = TickTackType.Cross
+                ttV!.elementColor = crossColor
+            }
+            isCircleTurn = !isCircleTurn
+        }
+    }
 
 }
 
---
+++ TicTacView.swift
@@ -0,0 +1,68 @@
+//
+//  TicTacView.swift
+//  TicTacToe
+//
+//  Created by Andreas Umbach on 09.08.2015.
+//  Copyright (c) 2015 Andreas Umbach. All rights reserved.
+//
+
+import UIKit
+
+
+enum TickTackType: Int {
+    case Empty = 0, Cross, Circle
+}
+
+@IBDesignable class TickTackView : UIView
+{
+    @IBInspectable var elementColor : UIColor = UIColor.redColor() {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
+    
+    var type : TickTackType = .Cross
+    
+    override func drawRect(rect: CGRect) {
+        let ctx = UIGraphicsGetCurrentContext()
+        
+        switch self.type
+        {
+        case .Circle:
+            let outerCircle = CGPathCreateMutable()
+            CGPathAddEllipseInRect(outerCircle, nil, CGRectInset(rect, rect.width * 0.1, rect.height * 0.1))
+            CGContextAddPath(ctx, outerCircle)
+            CGContextSetFillColorWithColor(ctx, elementColor.CGColor)
+            CGContextFillPath(ctx)
+            
+            let innerCircle = CGPathCreateMutable()
+            CGPathAddEllipseInRect(innerCircle, nil, CGRectInset(rect, rect.width * 0.2, rect.height * 0.2))
+            CGContextAddPath(ctx, innerCircle)
+            CGContextSetFillColorWithColor(ctx, UIColor.whiteColor().CGColor)
+            
+            CGContextFillPath(ctx)
+        case .Cross:
+            let path = CGPathCreateMutable()
+            let d : CGFloat = 0.1
+            let r = CGRectInset(rect, rect.width * 0.1, rect.height * 0.1)
+            var m = CGAffineTransformMakeTranslation(rect.width * 0.1, rect.height * 0.1)
+            CGPathMoveToPoint(path, &m, r.width * d, 0)
+            CGPathAddLineToPoint(path, &m, 0, r.height * d)
+            CGPathAddLineToPoint(path, &m, r.width * (1-d), r.height)
+            CGPathAddLineToPoint(path, &m, r.width, r.height * (1-d))
+            CGPathCloseSubpath(path)
+            CGPathMoveToPoint(path, &m, r.width * (1-d), 0)
+            CGPathAddLineToPoint(path, &m, 0, r.height * (1-d))
+            CGPathAddLineToPoint(path, &m, r.width * d, r.height)
+            CGPathAddLineToPoint(path, &m, r.width, r.height * d)
+            CGPathCloseSubpath(path)
+            
+            CGContextAddPath(ctx, path)
+            CGContextSetFillColorWithColor(ctx, elementColor.CGColor)
+            CGContextFillPath(ctx)
+        case .Empty:
+            break
+        }
+    }
+    
+}
